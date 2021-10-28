93ff70f435873975aed06f4f909d640dee8520d7
a19279653 <rseropalev@sberbank.ru>
2021-10-28 22:50:02 +0300

    add HW3

---
+++ HW3/src/main/scala/main.scala
@@ -0,0 +1,30 @@
+import breeze.linalg._
+import breeze.numerics.abs
+
+import java.io._
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    var df = csvread(new File(args(0)), ';', skipLines = 1)
+    var idx = (df.rows * 0.8).toInt // 80 на трейн и 20 процентов на валидацию
+    var (df_train, df_val) = (df(0 until idx, ::), df(idx until df.rows, ::))
+
+    var X_train = df_train(::, 1 to df_train.cols - 2)
+    var X_val = df_val(::, 1 to df_val.cols - 2)
+    var y_train = df_train(::, df_train.cols - 1)
+    var y_val = df_val(::, df_val.cols - 1)
+
+    var w = inv(X_train.t * X_train) * X_train.t * y_train
+    var pred = X_val * w
+    var MAE = sum(abs(pred - y_val)) / y_val.size
+    println("MAE on validation data = " + MAE.toString)
+
+    df = csvread(new File(args(1)), ';', skipLines = 1)
+    var X_test = df(::, 1 to df.cols - 2)
+    var y_test = df(::, df.cols - 1)
+    pred = X_test * w
+    MAE = sum(abs(pred - y_test)) / y_test.size
+    println("MAE on test data = " + MAE.toString)
+    csvwrite(new File("result.csv"), pred.toDenseMatrix, separator = '\n')
+  }
+}
